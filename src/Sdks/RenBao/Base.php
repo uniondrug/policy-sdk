9e5f95008a111d0032147bcb2d8b7acde62ce358
宇 <zoyulu@uniondrug.cn>
2018-08-07 09:53:58 +0800

    edit

--- src/Sdks/RenBao/Base.php
+++ src/Sdks/RenBao/Base.php
@@ -4,6 +4,11 @@ namespace Uniondrug\PolicySdk\Sdks\RenBao;
 
 use Uniondrug\PolicySdk\Sdk;
 
+/**
+ * 人保
+ * Class Base
+ * @package Uniondrug\PolicySdk\Sdks\RenBao
+ */
 class Base extends Sdk
 {
     public function __construct($name)
