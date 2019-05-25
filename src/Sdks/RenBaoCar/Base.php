65b4874933b3c67ea28954e1a3b06cc0c6c77cc6
lvzhonglin <lvzhonglin@uniondrug.cn>
2019-05-25 10:45:09 +0800

    edit

--- src/Sdks/RenBaoCar/Base.php
+++ src/Sdks/RenBaoCar/Base.php
@@ -73,20 +73,80 @@ class Base extends Sdk
      * @param $xml_content
      * @return array|mixed|\SimpleXMLElement
      */
-    public function getCurl($xml_content){
-        $url = $this->config->renbaoUrl;
-        $postJson = json_encode($xml_content, JSON_UNESCAPED_UNICODE);
-        $header = ['Content-Type: application/soap+xml'];
+    public function getCurl($request_content, $name, $transactionNo)
+    {
         try {
-            $result = $this->curl_https($url, $postJson, $header, __FUNCTION__,'120');
+            $xml_content = $this->getHead($transactionNo);
+            $xml_content .= $request_content;
+            $xml_content .= '<Sign>' . $this->getSign($request_content) . '</Sign>
+            </Package>
+        </PackageList>';
+            $url = $this->config->renbaoUrl;
+            $this->logger->{$name}()->info("保司请求报文:" . $xml_content);
+            $header = ['Content-Type: application/xml'];
+            $result = $this->curl_https($url, $xml_content, $header, $name, '120');
+            $this->logger->{$name}()->info("保司响应报文:" . $result);
+            $pattern = "/<Response>.*?<\/Response>/is";
+            preg_match($pattern,$result,$data);
+            //验签
+            $resultArray = xml_to_array($result, 'GB2312');
+            $this->checkSign($data['0'],$resultArray['Package']['Sign']);
         } catch (\Exception $e) {
-            return $this->withError($e->getMessage());
+            throw new \Exception($e->getMessage());
         }
-        $resultObj = xml_to_array($result, 'GB2312');
-        if( $resultObj['Package']['Header']['Status'] != 100 ){
-            return $this->withError($resultObj['Package']['Header']['ErrorMessage']);
-        }
-        return $resultObj;
+        return $resultArray;
     }
+
+    //加签验证
+    private function getSign($request)
+    {
+
+        $privateKey = file_get_contents(__DIR__ . "/rsa/our/rsa_private_key.pem");
+        $pkeyid = openssl_get_privatekey($privateKey);
+        if (empty($pkeyid)) {
+            throw new \Exception('获取签名失败!');
+        }
+        $verify = openssl_sign($request, $signature, $pkeyid, OPENSSL_ALGO_MD5);
+        openssl_free_key($pkeyid);
+        return base64_encode($signature);
+    }
+
+    //签名验证
+    private function checkSign($response, $sign)
+    {
+        $publicKey = file_get_contents(__DIR__ . "/rsa/our/rsa_public_key.pem");
+        $pkeyid = openssl_get_publickey($publicKey);
+        if (empty($pkeyid)) {
+            throw new \Exception('获取签名失败!');
+        }
+        $ret = openssl_verify($response, base64_decode($sign), $pkeyid, OPENSSL_ALGO_MD5);
+        if ($ret != 1) {
+            throw new \Exception('验签失败!');
+        }
+        return true;
+    }
+
+    //组装头部报文
+    private function getHead($transactionNo)
+    {
+
+        $head_content = '<?xml version="1.0" encoding="utf-8"?>
+        <PackageList>
+            <Package>
+                <Header>
+                    <Version>2</Version>
+                    <RequestType>101106</RequestType>
+                    <InsureType>100</InsureType>
+                    <SessionId>' . $transactionNo . '</SessionId>
+                    <SellerId>123456</SellerId>
+                    <From>DDINSURE</From>
+                    <SendTime>' . date('Y-m-d H:i:s', time()) . '</SendTime>
+                    <Status>100</Status>
+                    <ErrorMessage></ErrorMessage>
+                </Header>';
+
+        return $head_content;
+    }
+
 
 }
--- src/Sdks/RenBaoCar/Modules/DefaultQuote.php
+++ src/Sdks/RenBaoCar/Modules/DefaultQuote.php
@@ -4,21 +4,7 @@ namespace Uniondrug\PolicySdk\Sdks\RenBaoCar\Modules;
 
 trait DefaultQuote {
     public function defaultQuote(array $post){
-        $xml_content = '<?xml version="1.0" encoding="utf-8"?>
-        <PackageList>
-            <Package>
-                <Header>
-                    <Version>2</Version>
-                    <RequestType>101106</RequestType>
-                    <InsureType>100</InsureType>
-                    <SessionId>' . $post['transactionNo'] . '</SessionId>
-                    <SellerId>123456</SellerId>
-                    <From>DDINSURE</From>
-                    <SendTime>' . date('Y-m-d H:i:s',time()) . '</SendTime>
-                    <Status>100</Status>
-                    <ErrorMessage></ErrorMessage>
-                </Header>
-                <Request>
+        $request_content = '<Request>
                     <InputsList>
                         <Inputs type="insuredInfo">
                             <Input name="insuredIdNo">' . $post['insuredIdNo'] . '</Input>
@@ -35,7 +21,7 @@ trait DefaultQuote {
                             <Input name="bizBeginDate">' . $post['bizBeginDate'] . '</Input>
                             <Input name="bizBeginDateHour">' . $post['bizBeginDateHour'] . '</Input>
                             <Input name="forceBeginDate">' . $post['forceBeginDate'] . '</Input>
-                            <Input name="forceBeginDateHour">' . $post['forceBeginDateHour'] . '/Input>
+                            <Input name="forceBeginDateHour">' . $post['forceBeginDateHour'] . '</Input>
                             <Input name="loanFlag">' . $post['loanFlag'] . '</Input>
                             <Input name="checkCode">' . $post['checkCode'] . '</Input>
                             <Input name="checkCodeCI">' . $post['checkCodeCI'] . '</Input>
@@ -43,19 +29,23 @@ trait DefaultQuote {
                             <Input name="fueltype">' . $post['fueltype'] . '</Input>
                             <Input name="carprooftype">' . $post['carprooftype'] . '</Input>
                             <Input name="carproofno">' . $post['carproofno'] . '</Input>
-                            <Input name=" monopolycode "></Input>
-                            <Input name=" monopolyname "> </Input>
-                            <Input name=" subMonopolyType "></Input>
+                            <Input name=" monopolycode"></Input>
+                            <Input name=" monopolyname"></Input>
+                            <Input name=" subMonopolyType"></Input>
                         </Inputs>
-                    </InputsList>
-                </Request>
-                <Sign></Sign>
-            </Package>
-        </PackageList>';
-        $resultObj = $this->getCurl($xml_content);
+                    </InputsList> 
+                </Request>';
+        try {
+            $resultArray = $this->getCurl($request_content,__FUNCTION__,$post['transactionNo']);
+        } catch (\Exception $e) {
+            return $this->withError($e->getMessage());
+        }
+        if ($resultArray['Package']['Header']['Status'] != 100) {
+            return $this->withError($resultArray['Package']['Header']['ErrorMessage']);
+        }
         $data = [
-            'header' => $resultObj['Package']['Header'],
-            'data' => $resultObj['Package']['Response'],
+            'header' => $resultArray['Package']['Header'],
+            'data' => $resultArray['Package']['Response'],
         ];
         return $this->withData($data);
     }
